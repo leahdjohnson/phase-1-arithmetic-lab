4fad4dd824c42f0a155de3a287abd80888ffbb0c
Leah <elloworldt@gmail.com>
2021-11-12 16:34:46 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 15.5;
+let num2 = 4;
+let num3 = 64;
+let num4 = 15;
+
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = (num3 / num4)
+
+let max = Math.max(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
