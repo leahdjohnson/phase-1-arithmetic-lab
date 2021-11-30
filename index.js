c9d86130cf5ba009baadf2389c2637e514ac2389
Leah <elloworldt@gmail.com>
2021-11-29 16:47:37 -0800

    commit tree 40e422babcfc21fa331df36039b9a2f88aeada20

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 // Write your code here
-let num1 = 15.5;
-let num2 = 4;
-let num3 = 64;
-let num4 = 15;
+let num1 = 31
+let num2 = 2
+let num3 = 32
+let num4 = 8
 
-let multiply = (num1 * num2);
+let multiply = (num1 * num2)
 
 let random = Math.floor(Math.random() * 10) + 1;
 
-let mod = (num3 / num4)
+let mod = Math.round(num3 / num4)
 
 let max = Math.max(1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
